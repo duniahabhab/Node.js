12efee42a0a5b6bea323e5b639daa0a083cb28b7
duniahabhab <84604258+duniahabhab@users.noreply.github.com>
2022-02-13 23:43:13 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+
+console.log( "Hello!" );
+
+
+
+const languages = ['HTML', 'CSS', 'JavaScript'];
+
+const [markup, style, scripting] = languages;
+
+console.log(markup, style, scripting); // <= HTML CSS JavaScript
+console.log(markup); // <= HTML
